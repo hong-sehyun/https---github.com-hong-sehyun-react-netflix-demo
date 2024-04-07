437e9c9a738b9b5d449360c4278dbf9516a96b07
HONG SE HYUN <sese0733@naver.com>
2024-04-07 21:58:43 +0900

    feat: banner 제작

---
+++ src/pages/HomePage/components/Banner/Banner.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import CircularProgress from "@mui/material/CircularProgress";
+import Alert from "@mui/material/Alert";
+import "./Banner.style.css";
+
+const Banner = () => {
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+  console.log("ddd", data);
+  //  console.log(data.results[0].poster_path);
+  if (isLoading) {
+    <CircularProgress />;
+  }
+
+  if (isError) {
+    <Alert variant="filled" severity="error">
+      {error.message}
+    </Alert>;
+  }
+
+  if (!data || !data.results) {
+    return <div>Loading...</div>;
+  }
+
+  console.log("Poster path:", data.results[0].poster_path);
+
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}` +
+          ")",
+      }}
+      className="banner"
+    >
+      <div className="banner-text">
+        <span>{data?.results[0].title}</span>
+        <p>{data?.results[0].overview}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
