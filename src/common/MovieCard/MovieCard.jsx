aa23e13ea7378d4e699654db66795b4184a4cdf5
HONG SE HYUN <sese0733@naver.com>
2024-04-09 22:16:07 +0900

    feat: 장르 보여주기

---
+++ src/common/MovieCard/MovieCard.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Chip from "@mui/material/Chip";
+import "./MovieCard.style.css";
+import StarIcon from "@mui/icons-material/Star";
+import { useMovieGenreQuery } from "../../hooks/useMovieGenre";
+
+const MovieCard = ({ movie }) => {
+  const { data: genreData } = useMovieGenreQuery();
+
+  const showGenre = (genreIdList) => {
+    if (!genreData) return [];
+    const genreNameList = genreIdList.map((id) => {
+      const genreObj = genreData.find((genre) => genre.id === id);
+      return genreObj.name;
+    });
+
+    return genreNameList;
+  };
+
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${movie.poster_path}` +
+          ")",
+      }}
+      className="movie-card"
+    >
+      <div className="overlay">
+        <h3>{movie.title}</h3>
+        {showGenre(movie.genre_ids).map((id) => (
+          <Chip label={id} color="error" />
+        ))}
+        <div>
+          <div>
+            <StarIcon />
+            {movie.vote_average}
+          </div>
+          <div>{movie.popularity}</div>
+          <div>{movie.adult ? "18+" : ""}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
